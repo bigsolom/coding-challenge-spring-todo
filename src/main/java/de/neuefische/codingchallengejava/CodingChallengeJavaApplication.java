28e037adcb3aef2f3367b93a2acb9530bdaa8170
bigsolom <eslam.foad@gmail.com>
2023-05-01 12:23:13 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/codingchallengejava/CodingChallengeJavaApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.codingchallengejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChallengeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingChallengeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/codingchallengejava/CodingChallengeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.codingchallengejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChallengeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
